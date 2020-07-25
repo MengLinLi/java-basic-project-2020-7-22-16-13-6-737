585381100420649b7803e552fc101b9e10a0d81d
fan <610398596@qq.com>
2020-07-25 09:40:15 +0800

    should_return_30_when_given_normal_user_pos_amont_8_108_208

--- src/test/java/com/thoughtworks/basic/CreditCardPointsTest.java
+++ src/test/java/com/thoughtworks/basic/CreditCardPointsTest.java
@@ -52,6 +52,26 @@ public class CreditCardPointsTest {
                 "2020-07-02 18:40 POS机消费 208元，积分 +20",result);
     }
 
+    @Test
+    public void should_return_30_when_given_normal_user_pos_amont_8_108_208(){
+        //given
+        Consume consume1 = new Consume("POS机消费",new BigDecimal(8),"normal","2020-07-02 18:50");
+        Consume consume2 = new Consume("POS机消费",new BigDecimal(108),"normal","2020-07-02 18:40");
+        Consume consume3 = new Consume("POS机消费",new BigDecimal(208),"normal","2020-07-02 18:40");
+        List<Consume> consumes = new ArrayList<>();
+        consumes.add(consume1);
+        consumes.add(consume2);
+        consumes.add(consume3);
+        CreditCardPoints creditCardPoints = new CreditCardPoints(consumes);
+        //when
+        String  result=creditCardPoints.calculationPoints();
+        //then
+        Assert.assertEquals("总积分：30\n" +
+                "2020-07-02 18:50 POS机消费 8元，积分 +0\n"+
+                "2020-07-02 18:40 POS机消费 108元，积分 +10\n"+
+                "2020-07-02 18:40 POS机消费 208元，积分 +20",result);
+    }
+
 }
 
 
